c17a895dd014a90fcd190d35143019e6f7b264fc
danielghazaly5-del <danielghazaly5@gmail.com>
2026-05-24 15:19:00 +0300

    add config

---
+++ config.py
@@ -0,0 +1,10 @@
+DB_HOST = "localhost"
+DB_USER = "root"
+DB_PASSWORD = ""
+DB_NAME = "ai_tender_laws"
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+VECTOR_DB_PATH = os.path.join(BASE_DIR, "data", "vector_db")
