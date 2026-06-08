0ce2f5dcdf917aa2f2ae2a21201b0cd64c7fec28
danielghazaly5-del <danielghazaly5@gmail.com>
2026-06-08 22:54:26 +0300

    adjust config

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 import os
+from dotenv import load_dotenv
 
-DB_HOST = "localhost"
-DB_USER = "root"
-DB_PASSWORD = ""
-DB_NAME = "ai_tender_laws"
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_NAME = os.getenv("DB_NAME", "ai_tender_laws")
 
 
 
